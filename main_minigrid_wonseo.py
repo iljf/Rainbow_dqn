c155392e637bee026b910f00d9903bca9c990e3e
user name <wlwrl999@gmail.com>
2023-10-17 14:41:05 +0900

    need check for e-greedy

--- main_minigrid_wonseo.py
+++ main_minigrid_wonseo.py
@@ -32,17 +32,20 @@ parser.add_argument('--id', type=str, default='default', help='Experiment ID')
 parser.add_argument('--seed', type=int, default=123, help='Random seed')
 parser.add_argument('--disable-cuda', action='store_true', help='Disable CUDA')
 parser.add_argument('--game', type=str, default='MiniGrid-Empty-5x5-v0', help='minigrid')
-parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+# parser.add_argument('--T-max', type=int, default=int(50e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+parser.add_argument('--T-max', type=int, default=int(5e6), metavar='STEPS', help='Number of training steps (4x number of frames)')
+# parser.add_argument('--max-episode-length', type=int, default=int(108e3), metavar='LENGTH', help='Max episode length in game frames (0 to disable)')
 parser.add_argument('--max-episode-length', type=int, default=int(108e3), metavar='LENGTH', help='Max episode length in game frames (0 to disable)')
 parser.add_argument('--history-length', type=int, default=1, metavar='T', help='Number of consecutive states processed')
 parser.add_argument('--architecture', type=str, default='mlp', choices=['canonical', 'data-efficient', 'mlp'], metavar='ARCH', help='Network architecture')
 parser.add_argument('--hidden-size', type=int, default=512, metavar='SIZE', help='Network hidden size')
 parser.add_argument('--noisy-std', type=float, default=0.1, metavar='σ', help='Initial standard deviation of noisy linear layers')
 parser.add_argument('--atoms', type=int, default=51, metavar='C', help='Discretised size of value distribution')
-parser.add_argument('--V-min', type=float, default=-10, metavar='V', help='Minimum of value distribution support')
-parser.add_argument('--V-max', type=float, default=10, metavar='V', help='Maximum of value distribution support')
+parser.add_argument('--V-min', type=float, default=-2, metavar='V', help='Minimum of value distribution support')
+parser.add_argument('--V-max', type=float, default=2, metavar='V', help='Maximum of value distribution support')
 parser.add_argument('--model', type=str, metavar='PARAMS', help='Pretrained model (state dict)')
-parser.add_argument('--memory-capacity', type=int, default=int(1e6), metavar='CAPACITY', help='Experience replay memory capacity')
+# parser.add_argument('--memory-capacity', type=int, default=int(1e6), metavar='CAPACITY', help='Experience replay memory capacity')
+parser.add_argument('--memory-capacity', type=int, default=int(10e5), metavar='CAPACITY', help='Experience replay memory capacity')
 parser.add_argument('--replay-frequency', type=int, default=4, metavar='k', help='Frequency of sampling from memory')
 parser.add_argument('--priority-exponent', type=float, default=0.5, metavar='ω', help='Prioritised experience replay exponent (originally denoted α)')
 parser.add_argument('--priority-weight', type=float, default=0.4, metavar='β', help='Initial prioritised experience replay importance sampling weight')
@@ -50,18 +53,21 @@ parser.add_argument('--multi-step', type=int, default=3, metavar='n', help='Numb
 parser.add_argument('--discount', type=float, default=0.99, metavar='γ', help='Discount factor')
 parser.add_argument('--target-update', type=int, default=int(8e3), metavar='τ', help='Number of steps after which to update target network')
 parser.add_argument('--reward-clip', type=int, default=1, metavar='VALUE', help='Reward clipping (0 to disable)')
-parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
+# parser.add_argument('--learning-rate', type=float, default=0.0000625, metavar='η', help='Learning rate')
+parser.add_argument('--learning-rate', type=float, default=0.03, metavar='η', help='Learning rate')
 parser.add_argument('--adam-eps', type=float, default=1.5e-4, metavar='ε', help='Adam epsilon')
 parser.add_argument('--batch-size', type=int, default=32, metavar='SIZE', help='Batch size')
 parser.add_argument('--norm-clip', type=float, default=10, metavar='NORM', help='Max L2 norm for gradient clipping')
-parser.add_argument('--learn-start', type=int, default=int(20e3), metavar='STEPS', help='Number of steps before starting training')
-# parser.add_argument('--learn-start', type=int, default=int(1), metavar='STEPS', help='Number of steps before starting training')
+# parser.add_argument('--learn-start', type=int, default=int(20e3), metavar='STEPS', help='Number of steps before starting training')
+parser.add_argument('--learn-start', type=int, default=int(1e3), metavar='STEPS', help='Number of steps before starting training')
 parser.add_argument('--evaluate', action='store_true', help='Evaluate only')
+# parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
 parser.add_argument('--evaluation-interval', type=int, default=100000, metavar='STEPS', help='Number of training steps between evaluations')
-# parser.add_argument('--evaluation-interval', type=int, default=1, metavar='STEPS', help='Number of training steps between evaluations')
+# parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
 parser.add_argument('--evaluation-episodes', type=int, default=10, metavar='N', help='Number of evaluation episodes to average over')
 # TODO: Note that DeepMind's evaluation method is running the latest agent for 500K frames ever every 1M steps
-parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
+# parser.add_argument('--evaluation-size', type=int, default=500, metavar='N', help='Number of transitions to use for validating Q')
+parser.add_argument('--evaluation-size', type=int, default=5000, metavar='N', help='Number of transitions to use for validating Q')
 parser.add_argument('--render', action='store_true', help='Display screen (testing only)')
 parser.add_argument('--enable-cudnn', action='store_true', help='Enable cuDNN (faster but nondeterministic)')
 parser.add_argument('--checkpoint-interval', default=0, help='How often to checkpoint the model, defaults to 0 (never checkpoint)')
@@ -72,7 +78,8 @@ parser.add_argument('--disable-bzip-memory', action='store_true', help='Don\'t z
 args = parser.parse_args()
 
 # wandb intialize
-wandb.init(project="Rainbow_new_"+args.game,
+wandb.init(project="Rainbow_revision",
+           name=args.game+"test4",
            config=args.__dict__
            )
 
@@ -116,9 +123,12 @@ def save_memory(memory, memory_path, disable_bzip):
     with bz2.open(memory_path, 'wb') as zipped_pickle_file:
       pickle.dump(memory, zipped_pickle_file)
 
+# if np.random.rand() < eps:
+#     # do action that is random.
 
 # Environment
 env = gym.make(args.game)
+# env = gym.make(args.game, render_mode = 'human')
 # env = RGBImgPartialObsWrapper(env) # Get pixel observations
 env = FullyObsWrapper(env)
 env = ImgObsWrapper(env) # Get rid of the 'mission' field
@@ -152,8 +162,8 @@ priority_weight_increase = (1 - args.priority_weight) / (args.T_max - args.learn
 val_mem = ReplayMemory(args, args.evaluation_size)
 T, done = 0, True
 while T < args.evaluation_size:
-    if done:
-        state, _ = env.reset()
+    if done or truncated:
+        state, _ = env.reset(seed=42)
         state = torch.Tensor(state)
         # state = state.unsqueeze(0)
         done = False
@@ -165,19 +175,24 @@ while T < args.evaluation_size:
     state = next_state
     T += 1
 
-    wandb.log({'training/reward':r})
+    wandb.log({'Replay_memory/reward':r})
+    # wandb.log({'wandb 저장명': 값})
 
 if args.evaluate:
   dqn.eval()  # Set DQN (online network) to evaluation mode
   avg_reward, avg_Q = test_minigrid(args, 0, dqn, val_mem, metrics, results_dir, evaluate=True)  # Test
   print('Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
+
+  # wandb.log({'training/Q-value': avg_Q})
+
 else:
+
   # Training loop
   dqn.train()
   done = True
   for T in trange(1, args.T_max + 1):
     if done or truncated:
-      state, _ = env.reset()
+      state, _ = env.reset(seed=42)
       state = torch.Tensor(state).to(args.device)
       # done, truncated = False, False
       # state = state.unsqueeze(0)
@@ -192,12 +207,25 @@ else:
     next_state = torch.Tensor(next_state).to(args.device)
     # next_state = next_state.unsqueeze(0)
     # Step
+
+    # for _i in range(len(done)):
+    #     if done[_i]:
+    #         wandb.log(
+    #             {"training/episode/reward": info[_i]['returns/episodic_reward'],
+    #              "training/episode/length": info[_i]['returns/episodic_length']
+    #              })
+
     if args.reward_clip > 0:
       reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
     # mem.append(state, action, reward, done)  # Append transition to memory
     # if np.prod(state.shape)>75:
     #     print('w')
     mem.append(state.unsqueeze(0), action, reward, done)  # Append transition to memory
+
+    wandb.log({'training/reward': reward
+               })
+
+
 
     #check if mem is corrupted
     # for i in range(mem.transitions.data.shape[0]):
@@ -223,6 +251,12 @@ else:
         log('T = ' + str(T) + ' / ' + str(args.T_max) + ' | Avg. reward: ' + str(avg_reward) + ' | Avg. Q: ' + str(avg_Q))
         dqn.train()  # Set DQN (online network) back to training mode
 
+        wandb.log({'eval/reward' : reward,
+                   'eval/Average_reward': avg_reward,
+                   'eval/timestep': T,
+                   'eval/Q-value': avg_Q
+                   })
+
         # If memory path provided, save it
         if args.memory is not None:
           save_memory(mem, args.memory, args.disable_bzip_memory)
@@ -236,8 +270,5 @@ else:
         dqn.save(results_dir, 'checkpoint.pth')
 
     state = next_state
-    # wandb.log({'training/reward':r,
-    #            'training/weight':mem.priority_weight,
-    #            'training/Q_value':avg_Q})
 
 env.close()
